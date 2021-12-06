c64ab3ea1fb609cce0156b161bf78108011ea2c1
copsterr <siraphob@apipluspower.com>
2021-12-06 11:38:16 +0700

    add nvim

---
+++ nvim/nvim-setup.bash
@@ -0,0 +1,11 @@
+# install vim-plug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# install nerd-fonts (you still need to activate fonts in your terminal)
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
+# install nodejs
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
