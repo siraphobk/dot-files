7f66c99db0473e4d9e75a462c39bcc1047a28332
siraphobk <siraphob@apipluspower.com>
2022-08-01 13:45:50 +0700

    cleanup

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -37,9 +37,3 @@ map.set('n', '<leader>tr', function() nt_api.tree.reload() end)
 -- symbols-outline
 map.set('n', '<leader>ol', ':SymbolsOutline<CR>') -- show symbols-outline panel
 
-
--- Not sure what these do...
--- vim.cmd [[
---   inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
---   inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
--- ]]
