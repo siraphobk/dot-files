bf48613316eca554c5ebff87cbb962749053bbf6
siraphobk <siraphob.k@outlook.com>
2022-11-24 16:19:39 +0700

    edit sonokai colors

--- nvim/lua/themes/sonokai.lua
+++ nvim/lua/themes/sonokai.lua
@@ -9,5 +9,12 @@ endif
 vim.g.sonokai_style = 'shusia'
 vim.g.sonokai_better_performance = 1
 vim.g.sonokai_enable_italic = 1
+vim.g.sonokai_diagnostic_virtual_text = 'colored'
+
+vim.cmd [[ 
+  let g:sonokai_colors_override = {
+    \ 'bg0': ['#141414', '233']
+  \ }
+]]
 
 vim.cmd [[ colorscheme sonokai ]]
