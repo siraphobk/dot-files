9b7c5baa580496d6fe429e0dc6bc1e58d58a1457
siraphobk <siraphob.k@outlook.com>
2022-11-25 15:17:22 +0700

    healthcheck noice

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -180,6 +180,13 @@ return require("packer").startup(function(use)
       })
 
       require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
         routes = {
           {
             filter = {
