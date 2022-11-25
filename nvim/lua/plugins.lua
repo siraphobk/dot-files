d57e1a4414835ba0e7a3425e0fc96c49c7a88e93
siraphobk <siraphob.k@outlook.com>
2022-11-25 15:06:01 +0700

    add Noice!

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,174 +1,207 @@
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+  use("wbthomason/packer.nvim")
 
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = {
-			"nvim-tree/nvim-web-devicons", -- optional, for file icons
-		},
-	})
+  use({
+    "nvim-tree/nvim-tree.lua",
+    requires = {
+      "nvim-tree/nvim-web-devicons", -- optional, for file icons
+    },
+  })
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		requires = { { "nvim-lua/plenary.nvim" } },
-		run = "sudo apt install ripgrep",
-	})
+  use({
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    requires = { { "nvim-lua/plenary.nvim" } },
+    run = "sudo apt install ripgrep",
+  })
 
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		requires = {
-			{ "nvim-treesitter/nvim-treesitter-context" },
-		},
-	})
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    requires = {
+      { "nvim-treesitter/nvim-treesitter-context" },
+    },
+  })
 
-	use({
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup()
-		end,
-	})
+  use({
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup()
+    end,
+  })
 
-	use("williamboman/mason.nvim")
+  use("williamboman/mason.nvim")
 
-	use("neovim/nvim-lspconfig") -- Configurations for Nvim LSP
+  use("neovim/nvim-lspconfig") -- Configurations for Nvim LSP
 
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-vsnip")
-	use("hrsh7th/vim-vsnip")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-cmdline")
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-vsnip")
+  use("hrsh7th/vim-vsnip")
 
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	})
+  use({
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  })
 
-	use({ "akinsho/toggleterm.nvim", tag = "*" })
+  use({ "akinsho/toggleterm.nvim", tag = "*" })
 
-	use("ray-x/go.nvim")
-	use("ray-x/guihua.lua") -- recommended if need floating window support
+  use("ray-x/go.nvim")
+  use("ray-x/guihua.lua") -- recommended if need floating window support
 
-	-- THEMES
-	use("sainnhe/sonokai")
-	use("sainnhe/gruvbox-material")
+  -- THEMES
+  use("sainnhe/sonokai")
+  use("sainnhe/gruvbox-material")
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
 
-	use("simrat39/symbols-outline.nvim")
+  use("simrat39/symbols-outline.nvim")
 
-	use({
-		"mfussenegger/nvim-dap",
-		run = "go install github.com/go-delve/delve/cmd/dlv@latest",
-		requires = {
-			{ "leoluz/nvim-dap-go" },
-			{ "rcarriga/nvim-dap-ui" },
-		},
-	})
+  use({
+    "mfussenegger/nvim-dap",
+    run = "go install github.com/go-delve/delve/cmd/dlv@latest",
+    requires = {
+      { "leoluz/nvim-dap-go" },
+      { "rcarriga/nvim-dap-ui" },
+    },
+  })
 
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	})
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  })
 
-	use("lukas-reineke/indent-blankline.nvim")
+  use("lukas-reineke/indent-blankline.nvim")
 
-	use({
-		"romgrk/barbar.nvim",
-		requires = { "kyazdani42/nvim-web-devicons" },
-	})
+  use({
+    "romgrk/barbar.nvim",
+    requires = { "kyazdani42/nvim-web-devicons" },
+  })
 
-	use("rust-lang/rust.vim")
+  use("rust-lang/rust.vim")
 
-	-- Dashboard
-	use({
-		"goolord/alpha-nvim",
-		requires = { "kyazdani42/nvim-web-devicons" },
-		config = function()
-			require("alpha").setup(require("alpha.themes.startify").config)
-		end,
-	})
+  -- Dashboard
+  use({
+    "goolord/alpha-nvim",
+    requires = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      require("alpha").setup(require("alpha.themes.startify").config)
+    end,
+  })
 
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	})
+  use({
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  })
 
-	use({
-		"rmagatti/auto-session",
-		config = function()
-			require("auto-session").setup({
-				log_level = "error",
-				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-			})
-		end,
-	})
+  use({
+    "rmagatti/auto-session",
+    config = function()
+      require("auto-session").setup({
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      })
+    end,
+  })
 
-	use({
-		"windwp/nvim-spectre",
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
+  use({
+    "windwp/nvim-spectre",
+    requires = {
+      "nvim-lua/plenary.nvim",
+    },
 
-		config = function()
-			local spectre = require("spectre")
-			spectre.setup()
+    config = function()
+      local spectre = require("spectre")
+      spectre.setup()
 
-			vim.keymap.set("n", "<leader>So", function()
-				spectre.open()
-			end)
-			vim.keymap.set("n", "<leader>Sw", function()
-				spectre.open_visual({ select_word = true })
-			end)
-			vim.keymap.set("n", "<leader>Sf", function()
-				spectre.open_file_search()
-			end)
-		end,
-	})
+      vim.keymap.set("n", "<leader>So", function()
+        spectre.open()
+      end)
+      vim.keymap.set("n", "<leader>Sw", function()
+        spectre.open_visual({ select_word = true })
+      end)
+      vim.keymap.set("n", "<leader>Sf", function()
+        spectre.open_file_search()
+      end)
+    end,
+  })
 
-	use({ "jose-elias-alvarez/null-ls.nvim" })
+  use({ "jose-elias-alvarez/null-ls.nvim" })
 
-	use({
-		"karb94/neoscroll.nvim",
-		config = function()
-			require("neoscroll").setup()
-		end,
-	})
+  use({
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup()
+    end,
+  })
 
-	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({})
+  use({
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup({})
 
-			vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
-			vim.keymap.set(
-				"n",
-				"<leader>xw",
-				"<cmd>TroubleToggle workspace_diagnostics<cr>",
-				{ silent = true, noremap = true }
-			)
-			vim.keymap.set(
-				"n",
-				"<leader>xd",
-				"<cmd>TroubleToggle document_diagnostics<cr>",
-				{ silent = true, noremap = true }
-			)
-			vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
-			vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
-			vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
-		end,
-	})
+      vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+      vim.keymap.set(
+        "n",
+        "<leader>xw",
+        "<cmd>TroubleToggle workspace_diagnostics<cr>",
+        { silent = true, noremap = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>xd",
+        "<cmd>TroubleToggle document_diagnostics<cr>",
+        { silent = true, noremap = true }
+      )
+      vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+      vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+    end,
+  })
+
+  use({
+    "folke/noice.nvim",
+    config = function()
+      require("notify").setup({
+        timeout = 1000,
+      })
+
+      require("noice").setup({
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              kind = "",
+              find = "written",
+            },
+            opts = { skip = true },
+          },
+        },
+
+        presets = {
+          bottom_search = false,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+        }
+      })
+    end,
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  })
 end)
--- nvim/lua/themes/sonokai.lua
+++ nvim/lua/themes/sonokai.lua
@@ -11,10 +11,10 @@ vim.g.sonokai_better_performance = 1
 vim.g.sonokai_enable_italic = 1
 vim.g.sonokai_diagnostic_virtual_text = "colored"
 
-vim.cmd([[ 
-  let g:sonokai_colors_override = {
-    \ 'bg0': ['#141414', '233']
-  \ }
-]])
+-- vim.cmd([[
+--   let g:sonokai_colors_override = {
+--     \ 'bg0': ['#141414', '233']
+--   \ }
+-- ]])
 
 vim.cmd([[ colorscheme sonokai ]])
