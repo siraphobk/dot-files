7576e1a9381243eb535ed8601b41ae8c9cec2346
siraphobk <siraphob.k@outlook.com>
2022-11-27 20:32:13 +0700

    add bufferline setup

--- nvim/lua/plugins/configs/barbar.lua
+++ nvim/lua/plugins/configs/barbar.lua
@@ -1,3 +1,5 @@
+require("bufferline").setup()
+
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
@@ -43,17 +45,17 @@ local nvim_tree_events = require("nvim-tree.events")
 local bufferline_api = require("bufferline.api")
 
 local function get_tree_size()
-	return require("nvim-tree.view").View.width
+  return require("nvim-tree.view").View.width
 end
 
 nvim_tree_events.subscribe("TreeOpen", function()
-	bufferline_api.set_offset(get_tree_size())
+  bufferline_api.set_offset(get_tree_size())
 end)
 
 nvim_tree_events.subscribe("Resize", function()
-	bufferline_api.set_offset(get_tree_size())
+  bufferline_api.set_offset(get_tree_size())
 end)
 
 nvim_tree_events.subscribe("TreeClose", function()
-	bufferline_api.set_offset(0)
+  bufferline_api.set_offset(0)
 end)
