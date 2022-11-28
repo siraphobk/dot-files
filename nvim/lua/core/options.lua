8f9dabaa6cbe80dbfaa2b66b95a7994733463302
siraphobk <siraphob.k@outlook.com>
2022-11-28 14:17:37 +0700

    fix foldlevel

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -28,7 +28,7 @@ set.wrap = false
 set.foldmethod = "expr"
 set.foldexpr = "nvim_treesitter#foldexpr()"
 set.foldenable = false
-set.foldlevel = 30
+set.foldlevel = 99
 
 vim.cmd([[ set signcolumn=yes:2 ]])
 vim.cmd([[ set list listchars=tab:\ \ ,nbsp:␣,trail:•,extends:⟩,precedes:⟨ ]])
