65e7df0a92af81426321fbb04bcda595bdf96d99
siraphobk <siraphob.k@outlook.com>
2022-12-02 09:40:25 +0700

    add more settings to gruvbox

--- nvim/lua/themes/gruvbox-material.lua
+++ nvim/lua/themes/gruvbox-material.lua
@@ -7,3 +7,5 @@ vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_visual = "green background"
 vim.g.gruvbox_material_diagnostic_text_highlight = 1
 vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+vim.g.gruvbox_material_foreground = "material"
+vim.g.gruvbox_material_ui_contrast = "high"
