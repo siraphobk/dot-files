4aaf60013d5acd1cefe23f73052358bfadadda13
siraphobk <siraphob.k@outlook.com>
2022-12-27 17:44:24 +0700

    configure go

--- nvim/lua/plugins/configs/go-nvim.lua
+++ nvim/lua/plugins/configs/go-nvim.lua
@@ -2,6 +2,5 @@ local p_lsp = require("plugins.configs.lspconfig") -- internal plugin import
 
 require("go").setup({
 	luasnip = true,
-	lsp_cfg = true, -- use default lsp config
 	lsp_keymaps = p_lsp.set_keymaps,
 })
