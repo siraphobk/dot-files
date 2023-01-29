2a99b7d3d7b135d283c109e3c5544ad9ffe9f20d
siraphobk <siraphob.k@outlook.com>
2023-01-29 20:25:21 +0700

    edit markdown after

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,8 +3,10 @@ local map = vim.keymap
 map.set("n", "j", "gj")
 map.set("n", "k", "gk")
 
-vim.cmd([[setlocal wrap]])
-vim.cmd([[setlocal tw=80]])
-vim.cmd([[setlocal fo+=t]])
-vim.cmd([[setlocal linebreak]])
-vim.cmd([[setlocal spell]])
+vim.cmd([[
+  setlocal wrap
+  setlocal tw=80
+  setlocal fo+=t
+  setlocal linebreak
+  setlocal spell
+]])
