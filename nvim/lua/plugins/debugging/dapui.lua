8d12912e0ce0f0d0a5e18f7e5edd598b001d5f5a
siraphobk <siraphob.k@outlook.com>
2023-02-15 22:41:37 +0700

    fix dapui

--- nvim/lua/plugins/debugging/dapui.lua
+++ nvim/lua/plugins/debugging/dapui.lua
@@ -6,34 +6,101 @@ dap.listeners.after.event_initialized["dapui_config"] = function()
 end
 
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close({})
+  dapui.close()
 end
 
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close({})
+  dapui.close()
 end
 
-vim.api.nvim_create_user_command("DapUI", "lua require('dapui').toggle()", {})
+-- Custom commands -------------------------------------------------------------
+
+vim.api.nvim_create_user_command("DapUIToggle", function()
+  dapui.toggle()
+end, {})
+
+vim.api.nvim_create_user_command("DapUIAddToWatch", function()
+  local word = vim.fn.expand("<cword>")
+  local expr = vim.fn.input("Expression: ", word)
+  dapui.elements.watches.add(expr)
+end, {})
+
+vim.api.nvim_create_user_command("DapUIEval", function()
+  dapui.eval(nil, {
+    enter = true,
+  })
+end, {})
 
 vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
 
 dapui.setup({
-  layouts = {
-    {
-      elements = {
-        { id = "scopes", size = 0.25 },
-        "breakpoints",
-        "stacks",
-        "watches",
-      },
-      size = 40,
-      position = "left",
-    },
-    {
-      elements = { "repl" },
-      size = 0.25,
-      position = "bottom",
-    },
+  controls = {
+    element = "repl",
+    enabled = true,
+    icons = {
+      disconnect = "",
+      pause = "",
+      play = "",
+      run_last = "",
+      step_back = "",
+      step_into = "",
+      step_out = "",
+      step_over = "",
+      terminate = ""
+    }
   },
+  element_mappings = {},
+  expand_lines = true,
+  floating = {
+    border = "single",
+    mappings = {
+      close = { "q", "<Esc>" }
+    }
+  },
+  force_buffers = true,
+  icons = {
+    collapsed = "",
+    current_frame = "",
+    expanded = ""
+  },
+  layouts = { {
+    elements = { {
+      id = "scopes",
+      size = 0.25
+    }, {
+      id = "breakpoints",
+      size = 0.25
+    }, {
+      id = "stacks",
+      size = 0.25
+    }, {
+      id = "watches",
+      size = 0.25
+    } },
+    position = "left",
+    size = 40
+  }, {
+    elements = { {
+      id = "repl",
+      size = 0.5
+    }, {
+      id = "console",
+      size = 0.5
+    } },
+    position = "bottom",
+    size = 10
+  } },
+  mappings = {
+    edit = "e",
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    repl = "r",
+    toggle = "t"
+  },
+  render = {
+    indent = 1,
+    max_value_lines = 100
+  }
 })
