9a96bb7f5cc75719b4de9ad412f7555bdfeccba7
siraphobk <siraphob.k@outlook.com>
2023-04-02 19:26:37 +0700

    fix lualine and barbar

--- nvim/lua/plugins/ui/lualine.lua
+++ nvim/lua/plugins/ui/lualine.lua
@@ -9,10 +9,10 @@ local function diff_source()
   end
 end
 
-require("lualine").setup({
+require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = "catppuccin",
+    theme = 'catppuccin',
     component_separators = { left = "┊", right = "┊" },
     section_separators = { left = "", right = "" },
     disabled_filetypes = {
@@ -26,30 +26,30 @@ require("lualine").setup({
       statusline = 1000,
       tabline = 1000,
       winbar = 1000,
-    },
+    }
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = { 'mode' },
     lualine_b = {
-      "branch",
+      'branch',
       { "diff", source = diff_source },
-      "diagnostics",
+      'diagnostics',
     },
-    lualine_c = { "filename", "searchcount" },
-    lualine_x = { "filetype" },
-    lualine_y = { "progress" },
-    lualine_z = { "location" },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
     lualine_y = {},
-    lualine_z = {},
+    lualine_z = {}
   },
   tabline = {},
   winbar = {},
   inactive_winbar = {},
-  extensions = {},
-})
+  extensions = {}
+}
