5704a478531ed17c61c6db57fb0c7642bc2c40e2
siraphobk <siraphob.k@outlook.com>
2023-04-20 10:55:33 +0700

    Add autocommands

---
+++ nvim/lua/core/autocommands.lua
@@ -0,0 +1,23 @@
+local api = vim.api
+
+api.nvim_create_augroup('setft', {
+  clear = true,
+})
+
+api.nvim_create_autocmd('BufEnter', {
+  group = "setft",
+  pattern = { "Dockerfile*" },
+  desc = "set filetype for Dockerfile",
+  callback = function(v)
+    vim.bo.filetype = "dockerfile"
+  end
+})
+
+api.nvim_create_autocmd('BufEnter', {
+  group = "setft",
+  pattern = { "Tiltfile*" },
+  desc = "set filetype for Tiltfile",
+  callback = function(v)
+    vim.bo.filetype = "python"
+  end
+})
--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -1,3 +1,4 @@
 require("core.keymaps")
 require("core.options")
 require("core.user_commands")
+require("core.autocommands")
