f84d316e7dc3fb0a130a91b651e931e2a14a0871
siraphobk <siraphob.k@outlook.com>
2023-05-17 20:57:03 +0700

    Add vscode config block

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,21 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+if vim.g.vscode then
+  -- vscode configuration
+else
+  -- Neovim configuration
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
 
-require("core")
-require("lazy").setup("plugins")
-require("themes")
+  require("core")
+  require("lazy").setup("plugins")
+  require("themes")
+end
