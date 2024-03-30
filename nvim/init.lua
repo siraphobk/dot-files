d20d5ed00a7e633eb2efb99e9eb9e993695cdb35
siraphobk <siraphob.k@outlook.com>
2024-03-30 21:25:26 +0700

    formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,21 @@
 if vim.g.vscode then
-  -- vscode configuration
+-- vscode configuration
 else
-  -- Neovim configuration
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable",
-      lazypath,
-    })
-  end
-  vim.opt.rtp:prepend(lazypath)
+	-- Neovim configuration
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable",
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
 
-  require("core")
-  require("lazy").setup("plugins")
-  -- require("themes")
+	require("core")
+	require("lazy").setup("plugins")
+	-- require("themes")
 end
