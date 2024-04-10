6971634eb4e1c48580fb7fc9ac5b267f3a9b9486
siraphobk <siraphob.k@outlook.com>
2024-04-10 14:20:27 +0700

    add alias

--- git/alias.sh
+++ git/alias.sh
@@ -3,11 +3,12 @@
 # Reset all git aliases
 git config --get-regexp alias | awk '{print $1}' | xargs -I{} git config --unset {}
 
-git config --global alias.pu      "push origin HEAD"
-git config --global alias.p      "pull"
+git config --global alias.pu "push origin HEAD"
+git config --global alias.p "pull"
 git config --global alias.fuckit "reset --hard"
-git config --global alias.s      "status"
-git config --global alias.aa     "add -A"
-git config --global alias.c      "commit"
-git config --global alias.adog   "log --all --decorate --oneline --graph"
-git config --global alias.wip    "!git add -A && git commit -m 'work in progress'"
+git config --global alias.s "status"
+git config --global alias.aa "add -A"
+git config --global alias.c "commit"
+git config --global alias.co "checkout"
+git config --global alias.adog "log --all --decorate --oneline --graph"
+git config --global alias.wip "!git add -A && git commit -m 'work in progress'"
